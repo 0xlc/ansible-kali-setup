13338e633e4cefaca94917d8fa9ae328fb36a2a6
Luca Nunzi <luca.nunzi@colossusbets.com>
2018-09-19 14:39:26 +0100

    updated

--- roles/kali_setup/files/base16-luca.sh
+++ roles/kali_setup/files/base16-luca.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # base16-shell (https://github.com/chriskempson/base16-shell)
 # Base16 Shell template by Chris Kempson (http://chriskempson.com)
-# Hopscotch scheme by Jan T. Sott
+# Luca scheme 
 
 color00="32/29/31" # Base 00 - Black
 color01="dd/46/4c" # Base 08 - Red
